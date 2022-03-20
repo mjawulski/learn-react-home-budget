afd6bde90618228ef624fca9c8cfc95472f263a7
Michal Jawulski <mjawulski@gmail.com>
2022-03-20 13:34:25 +0100

    Introduced css modules

--- src/TransactionsSummary/TransactionsSummary.jsx
+++ src/TransactionsSummary/TransactionsSummary.jsx
@@ -1,10 +1,11 @@
-import { TransactionsTable } from './TransactionsTable/TransactionsTable';
+import { TransactionsTable } from "./TransactionsTable/TransactionsTable";
+import classes from "./styles.module.css";
 
 export function TransactionsSummary({ transactions }) {
-	return (
-		<>
-			<h4 className='titleName'>Transactions</h4>
-			<TransactionsTable transactions={transactions} />
-		</>
-	);
+  return (
+    <>
+      <h4 className={classes.titleName}>Transactions</h4>
+      <TransactionsTable transactions={transactions} />
+    </>
+  );
 }
--- src/TransactionsSummary/TransactionsTable/TransactionsTable.jsx
+++ src/TransactionsSummary/TransactionsTable/TransactionsTable.jsx
@@ -1,8 +1,9 @@
 import { TransactionsTableRow } from "../TransactionsTableRow/TransactionsTableRow";
+import classes from "./styles.module.css";
 
 export function TransactionsTable({ transactions }) {
   return (
-    <table>
+    <table className={classes.transactionsTable}>
       {transactions.map((transaction) => {
         return <TransactionsTableRow transaction={transaction} />;
       })}
