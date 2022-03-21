75f0d0b4b604ebd34651283811c38cbddb199704
Marcin Jasiński <alpinus1@gmail.com>
2022-03-21 12:20:25 +0100

    task 02 done

--- src/TransactionsSummary/TransactionsTable/TransactionsTable.jsx
+++ src/TransactionsSummary/TransactionsTable/TransactionsTable.jsx
@@ -1,12 +1,14 @@
-import { TransactionsTableRow } from "../TransactionsTableRow/TransactionsTableRow";
-import classes from "./styles.module.css";
+import { AmountSummary } from '../AmountSummary/AmountSummary';
+import { TransactionsTableRow } from '../TransactionsTableRow/TransactionsTableRow';
+import classes from './styles.module.css';
 
 export function TransactionsTable({ transactions }) {
-  return (
-    <table className={classes.transactionsTable}>
-      {transactions.map((transaction) => {
-        return <TransactionsTableRow transaction={transaction} />;
-      })}
-    </table>
-  );
+	return (
+		<table className={classes.transactionsTable}>
+			{transactions.map(transaction => {
+				return <TransactionsTableRow transaction={transaction} />;
+			})}
+			<AmountSummary transactions={transactions} />
+		</table>
+	);
 }
